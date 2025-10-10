d0f57133104506f71e277014171849b73f7b75b9
Phil Hofer <phil@sunfi.sh>
2025-10-10 14:04:14 -0700

    fuzz_test.go: use Go fuzzing
    Use the Go fuzzer for fuzz testing.
    Advantages include multithreading by default and
    coverage corpus persistence.
    
    This incidentally eliminates one dependency, so
    the changes include the result of running "go mod tidy."

--- fuzz_test.go
+++ fuzz_test.go
@@ -1,68 +1,31 @@
-//go:build fuzz
-
 package aegis_test
 
 import (
 	"bytes"
-	"os"
 	"testing"
-	"time"
 
 	"github.com/ericlagergren/aegis"
 	"github.com/ericlagergren/aegis/internal/ref"
-	rand "github.com/ericlagergren/saferand"
 )
 
-func TestFuzz(t *testing.T) {
-	t.Run("AEGIS-128L", func(t *testing.T) {
-		t.Parallel()
-
-		testFuzz(t, aegis.KeySize128L, aegis.NonceSize128L)
-	})
-	t.Run("AEGIS-256", func(t *testing.T) {
-		t.Parallel()
-
-		testFuzz(t, aegis.KeySize256, aegis.NonceSize256)
-	})
+func FuzzRef128L(f *testing.F) {
+	fuzzRef(f, aegis.KeySize128L, aegis.NonceSize128L)
 }
 
-func testFuzz(t *testing.T, keySize, nonceSize int) {
-	d := 2 * time.Second
-	if testing.Short() {
-		d = 10 * time.Millisecond
-	}
-	if s := os.Getenv("AEGIS_FUZZ_TIMEOUT"); s != "" {
-		var err error
-		d, err = time.ParseDuration(s)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-	tm := time.NewTimer(d)
+func FuzzRef256(f *testing.F) {
+	fuzzRef(f, aegis.KeySize256, aegis.NonceSize256)
+}
 
+func fuzzRef(f *testing.F, keySize, nonceSize int) {
 	key := make([]byte, keySize)
 	nonce := make([]byte, nonceSize)
-	plaintext := make([]byte, 1*1024*1024) // 1 MB
-	for i := 0; ; i++ {
-		select {
-		case <-tm.C:
-			t.Logf("iters: %d", i)
+	plaintext := make([]byte, 4096)
+	copy(plaintext, "hello, world.")
+	f.Add(key, nonce, plaintext)
+	f.Fuzz(func(t *testing.T, key, nonce, plaintext []byte) {
+		if len(nonce) != nonceSize || len(key) != keySize {
 			return
-		default:
 		}
-
-		if _, err := rand.Read(key); err != nil {
-			t.Fatal(err)
-		}
-		if _, err := rand.Read(nonce); err != nil {
-			t.Fatal(err)
-		}
-		n := rand.Intn(len(plaintext))
-		if _, err := rand.Read(plaintext[:n]); err != nil {
-			t.Fatal(err)
-		}
-		plaintext := plaintext[:n]
-
 		refAead, err := ref.New(key)
 		if err != nil {
 			t.Fatal(err)
@@ -83,7 +46,6 @@ func testFuzz(t *testing.T, keySize, nonceSize int) {
 			}
 			t.Fatalf("expected %#x, got %#x", wantCt, gotCt)
 		}
-
 		wantPt, err := refAead.Open(nil, nonce, wantCt, nil)
 		if err != nil {
 			t.Fatal(err)
@@ -95,5 +57,5 @@ func testFuzz(t *testing.T, keySize, nonceSize int) {
 		if !bytes.Equal(wantPt, gotPt) {
 			t.Fatalf("expected %#x, got %#x", wantPt, gotPt)
 		}
-	}
+	})
 }
