a17f995df1b555801c92ae7dfab2eb5dd60eed7e
Phil Hofer <phil@sunfi.sh>
2026-06-19 08:32:54 -0700

    fuzzing: use 'go test' built-in fuzzer

--- fuzz_test.go
+++ fuzz_test.go
@@ -1,99 +1,62 @@
-//go:build fuzz
-
 package aegis_test
 
 import (
 	"bytes"
 	"crypto/rand"
-	"os"
 	"testing"
-	"time"
 
 	"github.com/philhofer/aegis"
 	"github.com/philhofer/aegis/internal/ref"
 )
 
-func TestFuzz(t *testing.T) {
-	t.Run("AEGIS-128L", func(t *testing.T) {
-		t.Parallel()
-
-		testFuzz(t, aegis.KeySize128L, aegis.NonceSize128L)
-	})
-	t.Run("AEGIS-256", func(t *testing.T) {
-		t.Parallel()
-
-		testFuzz(t, aegis.KeySize256, aegis.NonceSize256)
-	})
+func Fuzz128L(f *testing.F) {
+	testFuzz(f, aegis.KeySize128L, aegis.NonceSize128L)
 }
 
-func testFuzz(t *testing.T, keySize, nonceSize int) {
-	d := 2 * time.Second
-	if testing.Short() {
-		d = 10 * time.Millisecond
-	}
-	if s := os.Getenv("AEGIS_FUZZ_TIMEOUT"); s != "" {
-		var err error
-		d, err = time.ParseDuration(s)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
-	tm := time.NewTimer(d)
+func Fuzz256(f *testing.F) {
+	testFuzz(f, aegis.KeySize256, aegis.NonceSize256)
+}
 
+func testFuzz(f *testing.F, keySize, nonceSize int) {
 	key := make([]byte, keySize)
 	nonce := make([]byte, nonceSize)
-	plaintext := make([]byte, 1*1024*1024) // 1 MB
-	for i := 0; ; i++ {
-		select {
-		case <-tm.C:
-			t.Logf("iters: %d", i)
-			return
-		default:
-		}
+	rand.Read(key)
+	rand.Read(nonce)
 
-		if _, err := rand.Read(key); err != nil {
-			t.Fatal(err)
-		}
-		if _, err := rand.Read(nonce); err != nil {
-			t.Fatal(err)
-		}
-		n := rand.Intn(len(plaintext))
-		if _, err := rand.Read(plaintext[:n]); err != nil {
-			t.Fatal(err)
-		}
-		plaintext := plaintext[:n]
-
-		refAead, err := ref.New(key)
+	f.Add("", "")
+	f.Add("plaintext", "ad")
+	f.Add("a very longer plaintext string w/o ad", "")
+	f.Add("", "just ad")
+	f.Fuzz(func(t *testing.T, plaintext, ad string) {
+		refcrypt, err := ref.New(key)
 		if err != nil {
 			t.Fatal(err)
 		}
-		gotAead, err := aegis.New(key)
+		ourcrypt, err := aegis.New(key)
 		if err != nil {
 			t.Fatal(err)
 		}
+		pt := []byte(plaintext)
+		at := []byte(ad)
 
-		wantCt := refAead.Seal(nil, nonce, plaintext, nil)
-		gotCt := gotAead.Seal(nil, nonce, plaintext, nil)
+		wantCt := refcrypt.Seal(nil, nonce, pt, at)
+		gotCt := ourcrypt.Seal(nil, nonce, pt, at)
 		if !bytes.Equal(wantCt, gotCt) {
-			for i, c := range gotCt {
-				if c != wantCt[i] {
-					t.Fatalf("bad value at index %d of %d (%d): %#x",
-						i, len(wantCt), len(wantCt)-i, c)
-				}
-			}
-			t.Fatalf("expected %#x, got %#x", wantCt, gotCt)
+			t.Fatal("didn't get identical ciphertext")
 		}
-
-		wantPt, err := refAead.Open(nil, nonce, wantCt, nil)
+		wantPt, err := refcrypt.Open(nil, nonce, wantCt, at)
 		if err != nil {
 			t.Fatal(err)
 		}
-		gotPt, err := gotAead.Open(nil, nonce, wantCt, nil)
+		gotPt, err := ourcrypt.Open(nil, nonce, gotCt, at)
 		if err != nil {
 			t.Fatal(err)
 		}
 		if !bytes.Equal(wantPt, gotPt) {
-			t.Fatalf("expected %#x, got %#x", wantPt, gotPt)
+			t.Fatal("didn't get identitcal output plaintext")
 		}
-	}
+		if !bytes.Equal(gotPt, pt) {
+			t.Fatal("didn't round-trip plaintext")
+		}
+	})
 }
